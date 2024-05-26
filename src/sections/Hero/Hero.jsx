9a5964fa4f509c3e3a273585ff7ad57f9cbe7fa0
Fahru Rojak <gfahru5@gmail.com>
2024-05-26 07:46:57 +0700

    update

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import styles from './HeroStyles.module.css';
 import heroImg from '../../assets/fahru.png';
 import sun from '../../assets/sun.svg';
@@ -19,13 +20,28 @@ function Hero() {
   const githubIcon = theme === 'light' ? githubLight : githubDark;
   const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
 
+  const [displayedText, setDisplayedText] = useState('');
+  const fullText = "Passionate about frontend development and data analytics, I excel at creating visually appealing, user-friendly interfaces and leveraging data to enhance user experiences.";
+
+  useEffect(() => {
+    let index = 0;
+    const timer = setInterval(() => {
+      setDisplayedText(fullText.slice(0, index));
+      index += 1;
+      if (index > fullText.length) {
+        clearInterval(timer);
+      }
+    }, 50); // Adjust typing speed here
+    return () => clearInterval(timer);
+  }, [fullText]);
+
   return (
     <section id="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
         <img
           src={heroImg}
           className={styles.hero}
-          alt="Profile picture of Harris Johnsen"
+          alt="Photo Fahru"
         />
         <img
           className={styles.colorMode}
@@ -36,28 +52,24 @@ function Hero() {
       </div>
       <div className={styles.info}>
         <h1>
-         Fahru
+          Fahru
           <br />
           Rojak
         </h1>
         <h2>Frontend Developer</h2>
         <h3>DATA ENTHUSIAST</h3>
         <span>
-          <a href="https://instagram.com/fahruphoto" target="_blank">
+          <a href="https://instagram.com/fahruphoto" target="_blank" rel="noopener noreferrer">
             <img src={twitterIcon} alt="Twitter icon" />
           </a>
-          <a href="https://github.com/fahrurojak" target="_blank">
+          <a href="https://github.com/fahrurojak" target="_blank" rel="noopener noreferrer">
             <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="https://linkedin.com/fahrurojak" target="_blank">
+          <a href="https://linkedin.com/fahrurojak" target="_blank" rel="noopener noreferrer">
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
-        <p className={styles.description}>
-         Passionate about frontend development and data analytics,
-         I excel at creating visually appealing,
-         user-friendly interfaces and leveraging data to enhance user experiences. 
-        </p>
+        <p className={`${styles.description} ${styles.typewriter}`}>{displayedText}</p>
         <a href={CV} download>
           <button className="hover">Resume</button>
         </a>
