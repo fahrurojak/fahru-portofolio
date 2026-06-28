5079b9a5a668737cdb339c7d78b2697d6eb99d19
Fahru Rojak <gfahru5@gmail.com>
2026-06-28 12:15:36 +0700

    Add Portal Dittek project to portfolio

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -6,6 +6,7 @@ import book from '../../assets/book.png';
 import minds from '../../assets/minds.png';
 import triftingan from '../../assets/triftingan.png';
 import kimiafarma from '../../assets/kimiafarma.png';
+import ptdi from '../../assets/ptdi.png';
 import kateringgo from '../../assets/kateringgo.png';
 import ProjectCard from '../../common/ProjectCard';
 import notes from '../../assets/notes.png';
@@ -21,6 +22,7 @@ function Projects() {
     { src: minds, link: "https://minds-fahrurojaks-projects.vercel.app/", h3: "Minds", p: "Aplikasi kesehatan mental dengan fitur jurnal harian, meditasi terbimbing, dan pelacakan mood.", images: [minds] },
     { src: kateringgo, link: "https://katering-go.vercel.app/", h3: "KateringGO", p: "Sistem pemesanan katering pintar untuk acara kantoran dan keluarga dengan pilihan menu fleksibel.", images: [kateringgo] },
     { src: kimiafarma, link: "https://kfacenter.vercel.app/", h3: "Kimia Farma Center", p: "Sistem pendukung aktivitas operasional TI (KFA Center) melalui pengelolaan insiden, inventaris aset, jadwal maintenance, dan dashboard monitoring.", images: [kimiafarma] },
+    { src: ptdi, link: "https://portal-dittek.vercel.app/", h3: "Portal Dittek", p: "Sistem portal direktorat teknologi PT Dirgantara Indonesia.", images: [ptdi] },
     { src: property, link: "https://lookerstudio.google.com/reporting/611c3feb-aaea-4d6a-a843-836f731595e2/page/LQ2jD", h3: "Newyork Sales Property", p: "Dashboard visualisasi data interaktif menggunakan Looker Studio untuk analisis tren properti di New York.", images: [property] },
     { src: notes, link: "https://notes-app-fahrurojaks-projects.vercel.app/", h3: "Notes App", p: "Aplikasi catatan produktivitas super bersih yang mendukung sinkronisasi cepat dan pengkategorian cerdas.", images: [notes] }
   ];
